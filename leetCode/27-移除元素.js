2506e99f45b2e96ceca19545e3dd4198fa9e8d63
Rachelmu <631032057@qq.com>
2024-04-22 00:52:23 +0800

    移除元素

--- leetCode/27-移除元素.js
+++ leetCode/27-移除元素.js
@@ -9,3 +9,14 @@ var removeElement = (nums, val) => {
     }
     return k;
 };
+
+function removeElement(nums: number[], val: number): number {
+    let slowIndex: number = 0, fastIndex: number = 0;
+    while (fastIndex < nums.length) {
+        if (nums[fastIndex] !== val) {
+            nums[slowIndex++] = nums[fastIndex];
+        }
+        fastIndex++;
+    }
+    return slowIndex;
+};
